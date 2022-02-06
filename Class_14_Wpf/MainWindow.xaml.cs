71dab99ea86b0e64e45628c8cc1d9ab17cc74ad7
Irina <spartak.irina92@gmail.com>
2022-02-06 21:04:43 +0700

    Добавьте файлы проекта.

---
+++ Class_14_Wpf/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Class_14_Wpf
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public List<Product> products;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            products = new List<Product>();
+
+            products.Add(new Product()
+            {
+                ProductName = "Mango",
+                Price = 250,
+                ImageName = "Data/mango.jpg",
+                ProductType = ProductTypes.Food
+            });
+
+            products.Add(new Product()
+            {
+                ProductName = "Orange",
+                Price = 180,
+                ImageName = "Data/orange.jpg",
+                ProductType = ProductTypes.Food
+            });
+            products.Add(new Product()
+            {
+                ProductName = "Laptop",
+                Price = 160000,
+                ImageName = "Data/laptop.jpg",
+                ProductType = ProductTypes.Technic
+            });
+            products.Add(new Product()
+            {
+                ProductName = "TV",
+                Price = 200,
+                ImageName = "Data/tv.jpeg",
+                ProductType = ProductTypes.Technic
+            });
+            lstBox.ItemsSource = products;
+        }
+    }
+}
---
+++ Class_14_Wpf/Product.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_14_Wpf
+{
+    public enum ProductTypes
+    {
+        Food,
+        Technic
+    }
+
+    public class Product
+    {
+        public string ProductName { get; set; } // название продукта
+        public int Price { get; set; } // цента товара 
+        public string ImageName { get; set; }         // число определ. приоритет 
+        public ProductTypes ProductType { get; set; } // тип продуктов (вынесен выше и доступен всем) 
+
+
+        public override string ToString()
+        {
+            return ProductName; // возвращаем название продукта
+        }
+    }
+}
